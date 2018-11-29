6fdcdf7120083ef77a9be732f3025218d4bab5b3
Vale <38182450+itsVale@users.noreply.github.com>
2018-11-29 22:06:20 +0100

    This one too

--- cogs/special/dreamhub.py
+++ cogs/special/dreamhub.py
@@ -14,7 +14,7 @@ DREAMHUB_INFO_CHANNEL = 512290391220027433
 class DreamhubExclusive:
     def __init__(self, bot):
         self.bot = bot
-        await self.bot.wait_until_ready()  # To prevent errors in case this cog will be loaded before the Fun cog
+        await self.bot.wait_until_ready()
 
         client = IdiotClient(bot.idiotic_api_key, dev=True, session=bot.session)
         self.get_image = client.retrieve_greeting
